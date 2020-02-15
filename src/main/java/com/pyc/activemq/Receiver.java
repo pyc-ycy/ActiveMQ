ae27fc3aab3622f658cefd939df9e928611a8bc0
ycy-ycy <2923616405@qq.com>
2020-02-15 11:44:52 +0800

    update

--- src/main/java/com/pyc/activemq/Receiver.java
+++ src/main/java/com/pyc/activemq/Receiver.java
@@ -7,5 +7,13 @@
 
 package com.pyc.activemq;
 
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Receiver {
+    @JmsListener(destination = "my-destination")
+    public void receiverMessage(String message){
+        System.out.println("接收到：<" + message + ">");
+    }
 }
