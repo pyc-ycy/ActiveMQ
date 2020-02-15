140b4c2c59906c4e813f73ee9f3613c41318fae6
ycy-ycy <2923616405@qq.com>
2020-02-15 11:42:12 +0800

    update

--- src/main/java/com/pyc/activemq/ActivemqApplication.java
+++ src/main/java/com/pyc/activemq/ActivemqApplication.java
@@ -1,10 +1,22 @@
 package com.pyc.activemq;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.core.JmsTemplate;
 
 @SpringBootApplication
-public class ActivemqApplication {
+public class ActivemqApplication implements CommandLineRunner {
+    // implement CommandLinerRunner and overwrite Run method
+    //so that can execute our code when program start
+    @Autowired
+    JmsTemplate jmsTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+        jmsTemplate.send("my-destination", new Msg());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ActivemqApplication.class, args);
---
+++ src/main/java/com/pyc/activemq/Receiver.java
@@ -0,0 +1,11 @@
+//IntelliJ IDEA
+//ActiveMQ
+//Receiver
+//2020/2/15
+// Author:御承扬
+//E-mail:2923616405@qq.com
+
+package com.pyc.activemq;
+
+public class Receiver {
+}
